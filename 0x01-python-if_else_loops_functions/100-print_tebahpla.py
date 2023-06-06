eef8e1dbf661e270748605d180358316bf878593
Fadyy22 <fadyalaa441@gmail.com>
2023-06-06 18:41:47 +0300

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in reversed(range(97, 123)):
-    print(f"{i - 32:c}" if i % 2 != 0 else f"{i:c}", end='')
+    print("{:c}".format(i - 32) if i % 2 != 0 else "{:c}".format(i), end='')
