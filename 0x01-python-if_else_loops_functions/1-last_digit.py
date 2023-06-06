72fb95f50a4da3ad59ea602a15ba25b960ad181a
Fadyy22 <fadyalaa441@gmail.com>
2023-06-06 18:13:28 +0300

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    lastDigit = (number * -1) % 10
+    lastDigit = ((number * -1) % 10) * -1
 else:
     lastDigit = number % 10
 
