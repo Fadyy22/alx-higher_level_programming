e32019cb960815cb1b3ecda8e6c4b0ec79552136
Fadyy22 <fadyalaa441@gmail.com>
2023-06-15 03:03:26 +0300

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string is None:
+    if type(roman_string) != str or roman_string == "":
         return 0
     sum = 0
     flag = 0
