b4e9626411c39c72306af6cb477e59eb33d7492b
Fadyy22 <fadyalaa441@gmail.com>
2023-06-15 03:33:46 +0300

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == "":
+    if type(roman_string) != str or roman_string is None:
         return 0
+
+    roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                     'D': 500, 'M': 1000}
     sum = 0
-    flag = 0
-    for char in roman_string:
-        if char == 'I':
-            flag += 1
-        elif char == 'V':
-            sum += 5
-        elif char == 'X':
-            sum += 10
-        elif char == 'L':
-            sum += 50
-        elif char == 'C':
-            sum += 100
-        elif char == 'D':
-            sum += 500
-        elif char == 'M':
-            sum += 1000
-    if roman_string[0] == 'I':
-        sum -= 1
-    else:
-        sum += flag
+    i = 0
+    while i < len(roman_string):
+        value = roman_numbers[roman_string[i]]
+        if (i + 1) < len(roman_string):
+            if value >= roman_numbers[roman_string[i + 1]]:
+                sum += value
+                i += 1
+            else:
+                sum -= value
+                i += 1
+        else:
+            sum += value
+            i += 1
     return sum
