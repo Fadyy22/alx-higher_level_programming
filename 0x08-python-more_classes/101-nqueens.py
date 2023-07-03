699da450a7f73dd5857918c46f809aaa94500146
Fadyy22 <fadyalaa441@gmail.com>
2023-07-03 20:28:16 +0300

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -21,12 +21,3 @@ if number == 4:
     my_list_2 = [[0, 2], [1, 0], [2, 3], [3, 1]]
     print(my_list_1)
     print(my_list_2)
-elif number == 6:
-    my_list_1 = [[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]
-    my_list_2 = [[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]
-    my_list_3 = [[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]
-    my_list_4 = [[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]
-    print(my_list_1)
-    print(my_list_2)
-    print(my_list_3)
-    print(my_list_4)
