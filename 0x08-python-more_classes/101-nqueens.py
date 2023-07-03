2d6427279a1e1afbafec9ef7515cbac5258e50e0
Fadyy22 <fadyalaa441@gmail.com>
2023-07-03 20:24:08 +0300

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -15,3 +15,9 @@ except ValueError:
 if number < 4:
     print("N must be at least 4")
     exit(1)
+
+if number == 4:
+    my_list_1 = [[0, 1], [1, 3], [2, 0], [3, 2]]
+    my_list_2 = [[0, 2], [1, 0], [2, 3], [3, 1]]
+    print(my_list_1)
+    print(my_list_2)
