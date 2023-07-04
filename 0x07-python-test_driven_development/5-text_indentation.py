28067e19126219a86e4b654513e84ddb0eeeac15
Fadyy22 <fadyalaa441@gmail.com>
2023-07-04 20:54:05 +0300

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+
+a module containing a text_indentation function
+
+"""
+
+
+def text_indentation(text):
+    """a function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+
+    Args:
+        text (str): text to perform indentation on
+
+    Raises:
+        TypeError: if text is not a string
+    """
+    new_text = ""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        if i == len(text) - 1:
+            new_text += text[i]
+        elif text[i] == " " and text[i - 1] in (".", "?", ":"):
+            new_text += "\n\n"
+        else:
+            new_text += text[i]
+    print(new_text, end="")
