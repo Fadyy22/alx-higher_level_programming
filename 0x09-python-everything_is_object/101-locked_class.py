aaf889410d2e370817760b8f7e68ecaa0cb6768a
Fadyy22 <fadyalaa441@gmail.com>
2023-07-05 02:09:28 +0300

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""a module containing class LockedClass """
+
 
 class LockedClass:
+    """
+    a class LockedClass with no class or object attribute,
+    that prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_nam
+    """
     def __setattr__(self, name, value):
         if name == "first_name":
             super().__setattr__(name, value)
