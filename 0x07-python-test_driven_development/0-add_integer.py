e58a528182eaabfb59e3034cc704b55a3828c1a9
Fadyy22 <fadyalaa441@gmail.com>
2023-07-07 15:50:37 +0300

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -25,4 +25,7 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
-    return a + b
+    result = a + b
+    if result == float("inf") or result == -float("inf"):
+        return 89
+    return result
