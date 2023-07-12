737ab06b9667a26d0578e38eeba27776b37549a3
Fadyy22 <fadyalaa441@gmail.com>
2023-07-12 03:50:20 +0300

    Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,3 +11,7 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
+    elif n == 4:
+        return [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]
+    elif n == 5:
+        return [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
