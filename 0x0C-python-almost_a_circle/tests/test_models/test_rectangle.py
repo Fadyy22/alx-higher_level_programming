da51bd6c0ddab6c6a050856dc2c47e7690d8cda1
Fadyy22 <fadyalaa441@gmail.com>
2023-07-18 02:23:51 +0300

    add doc for rec tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -18,6 +18,7 @@ class RectangleTest(unittest.TestCase):
         sys.stdout = self.output
 
     def test_width_height_x_y_non_int(self):
+        """tests width height x y not int"""
         with self.assertRaises(TypeError):
             rec = Rectangle(1.5, 15)
         with self.assertRaises(TypeError):
@@ -28,6 +29,7 @@ class RectangleTest(unittest.TestCase):
             rec1 = Rectangle(15, 15, 1, 2.1)  # type: ignore
 
     def test_width_height_less_than_or_equal_zero(self):
+        """tests width height <= 0"""
         with self.assertRaises(ValueError):
             rec = Rectangle(-5, 10)
         with self.assertRaises(ValueError):
@@ -38,35 +40,42 @@ class RectangleTest(unittest.TestCase):
             rec = Rectangle(5, 0)
 
     def test_x_y_less_than_zero(self):
+        """tests x y < 0"""
         with self.assertRaises(ValueError):
             rec = Rectangle(5, 10, -5, 2)
         with self.assertRaises(ValueError):
             rec = Rectangle(5, 10, 2, -4)
 
     def test_area(self):
+        """tests area function"""
         rec = Rectangle(10, 15)
         self.assertEqual(rec.area(), 150)
 
     def test_str_(self):
+        """tests __str__"""
         rec = Rectangle(10, 15, 7, 6)
         self.assertEqual(rec.__str__(), "[Rectangle] (1) 7/6 - 10/15")
 
     def test_display(self):
+        """tests dispaly function with only width and height"""
         rec = Rectangle(2, 4)
         rec.display()
         self.assertEqual(self.output.getvalue(), "##\n##\n##\n##\n")
 
     def test_display_with_x(self):
+        """tests dispaly function with only width and height and x"""
         rec = Rectangle(2, 4, 2, 0)
         rec.display()
         self.assertEqual(self.output.getvalue(), "  ##\n  ##\n  ##\n  ##\n")
 
     def test_display_with_x_and_y(self):
+        """tests dispaly function with only width and height and x and y"""
         rec = Rectangle(2, 4, 2, 1)
         rec.display()
         self.assertEqual(self.output.getvalue(), "\n  ##\n  ##\n  ##\n  ##\n")
 
     def test_args_update(self):
+        """tests update function with args"""
         rec = Rectangle(10, 10, 10, 10)
         rec.update(89, 15, 20, 13, 11)
         print(rec)
@@ -74,6 +83,7 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(self.output.getvalue(), msg)
 
     def test_kwargs_update(self):
+        """tests udpate function with kwargs"""
         rec = Rectangle(10, 10, 10, 10)
         rec.update(id=13, x=15, y=12, width=5, height=8)
         print(rec)
@@ -81,12 +91,14 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(self.output.getvalue(), msg)
 
     def test_to_dictionary(self):
+        """tests to_dictionary function"""
         rec = Rectangle(10, 2, 1, 9)
         rec_dictionary = rec.to_dictionary()
         my_dect = {"id": 1, "width": 10, "height": 2, "x": 1, "y": 9}
         self.assertEqual(rec_dictionary, my_dect)
 
     def test_to_json_string(self):
+        """test to_json_string function"""
         rec = Rectangle(10, 2, 1, 9)
         rec_dictionary = rec.to_dictionary()
         rec_list = Rectangle.to_json_string([rec_dictionary])
@@ -94,12 +106,14 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(rec_list, json_string)
 
     def test_to_json_string_none(self):
+        """tests to_json_string using None"""
         rec_list = Rectangle.to_json_string(None)
         print(rec_list)
         json_string = "[]\n"
         self.assertEqual(self.output.getvalue(), json_string)
 
     def test_from_json_string(self):
+        """tests from_json_string function"""
         list_input = [{"id": 89, "width": 10, "height": 4}]
         json_list_input = Rectangle.to_json_string(list_input)
         list_output = Rectangle.from_json_string(json_list_input)
@@ -107,6 +121,7 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(list_output, result)
 
     def test_from_json_string_none(self):
+        """tests from_json_string using None"""
         list_output = Rectangle.from_json_string(None)
         result = []
         self.assertEqual(list_output, result)
