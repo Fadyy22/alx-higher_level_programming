9c6785a8496b7c7a94f72592779aa0169b87df2f
Fadyy22 <fadyalaa441@gmail.com>
2023-08-16 21:55:25 +0300

    show user grants

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- list all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
