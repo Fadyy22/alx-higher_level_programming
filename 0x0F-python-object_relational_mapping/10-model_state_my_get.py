0bf70fdee5a4e4ddacf8146f02b5e28dea986005
Fadyy22 <fadyalaa441@gmail.com>
2023-09-15 17:47:51 +0300

    print state id from user input

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that prints the State object
+with the name passed as argument from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, or_
+from sys import argv
+
+if __name__ == "__main__":
+    connection_string = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(connection_string)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == f"{argv[4]}").first()
+    if state is None:
+        print("Not Found")
+    else:
+        print(state.id)
