6e2a9d42c6c3cca3d81a687a19b16ab1b5d6cc2d
Fadyy22 <fadyalaa441@gmail.com>
2023-09-15 13:55:37 +0300

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa"""
+"""script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
 import sys
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s", (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name= '{}'".format(sys.argv[4]))
     state = cur.fetchone()
     print(state)
