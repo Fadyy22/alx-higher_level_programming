d9dbea4f0e64f5f052fb7cf9529f7a40f6fa8e76
Fadyy22 <fadyalaa441@gmail.com>
2023-09-15 18:34:27 +0300

    update state name

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+if __name__ == "__main__":
+    connection_string = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(connection_string)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
