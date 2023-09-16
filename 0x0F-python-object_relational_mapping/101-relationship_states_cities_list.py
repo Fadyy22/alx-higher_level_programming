1daed465a7818457b02432e012cfad64092707bd
Fadyy22 <fadyalaa441@gmail.com>
2023-09-16 15:18:07 +0300

    join two tables

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that prints all City objects from the database hbtn_0e_14_usatou"""
+
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+if __name__ == "__main__":
+    connection_string = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(connection_string)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).join(City).order_by(State.id, City.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
