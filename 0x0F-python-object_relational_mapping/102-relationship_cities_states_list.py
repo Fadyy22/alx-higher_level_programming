6cbba8f6056ce2f15fef282d6f5cabdef180f961
Fadyy22 <fadyalaa441@gmail.com>
2023-09-16 15:29:41 +0300

    get cities with their states

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that prints all City objects from the database hbtn_0e_14_usatou"""
+
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+if __name__ == "__main__":
+    connection_string = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(connection_string)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).outerjoin(State).order_by(City.id).all()
+    print(cities)
+    for city in cities:
+        print(f"{city.id}: {city.name} -> {city.state.name}")
