e1a58dfb75fb25620819ef5c22e46b062808cbc5
Fadyy22 <fadyalaa441@gmail.com>
2023-09-17 20:34:03 +0300

    statement safe from sqli

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name=%s;", (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
     states = cur.fetchall()
     for state in states:
         print(state)
