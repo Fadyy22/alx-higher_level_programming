7d718133c55c11bef03eade40084b2850d98d4ff
Fadyy22 <fadyalaa441@gmail.com>
2023-09-29 00:47:03 +0300

    find peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -17,11 +17,3 @@ def find_peak(list_of_integers):
     for i in range(1, len(li) - 1):
         if li[i] >= li[i - 1] and li[i] >= li[i + 1]:
             return li[i]
-
-
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
