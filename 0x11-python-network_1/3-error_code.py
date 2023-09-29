2f1c8edcc6ab5ac39996d441cd8559fe0dd84f0b
Fadyy22 <fadyalaa441@gmail.com>
2023-09-29 16:24:39 +0300

    handle errors

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
         with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode("utf-8"))
     except error.HTTPError as err:
-        print(f"Error Code: {err.code}")
+        print(f"Error code: {err.code}")
