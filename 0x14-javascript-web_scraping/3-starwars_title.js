b0436b149cd3c1d4d6ed58b1eb2b559b27e99be7
Fadyy22 <fadyalaa441@gmail.com>
2023-10-24 16:01:09 +0300

    get movie title from star wars api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 
-const api = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`
+const api = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
 request.get(api, (err, res, body) => {
-  console.log(JSON.parse(body).title);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
