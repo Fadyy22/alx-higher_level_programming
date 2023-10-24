dffdb621129a37f2675265e81aa0425bee254357
Fadyy22 <fadyalaa441@gmail.com>
2023-10-24 16:21:00 +0300

    number of movies for char id 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
 
-const api = 'https://swapi-api.alx-tools.com/api/people/18';
+const api = process.argv[2];
+let noMovies = 0;
 
 request.get(api, (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
-    console.log((JSON.parse(body).films).length);
+    const results = JSON.parse(body).results;
+    for (const movie of results) {
+      for (const character of movie.characters) {
+        if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
+          noMovies += 1;
+        }
+      }
+    }
   }
+  console.log(noMovies);
 });
