69693c97ff13e0a8df49e89979c062604854aacd
Fadyy22 <fadyalaa441@gmail.com>
2023-10-25 03:11:06 +0300

    get characters in a movie in order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,19 +3,22 @@ const request = require('request');
 
 const moviesAPI = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-request(moviesAPI, (err, res, body) => {
+request(moviesAPI, async (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
     const characters = JSON.parse(body).characters;
 
     for (const character of characters) {
-      request(character, (err, res, body) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
+      await new Promise((resolve, reject) => {
+        request(character, async (err, res, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
       });
     }
   }
